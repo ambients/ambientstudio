f359fd24de6129c526b95bc4689bef3b42da4cda
Lee, Hyun Ki <hlee389@gatech.edu>
2018-06-05 10:07:40 -0400

    dimensions.ts
    dimensions.ts

--- src/dimensions.ts
+++ src/dimensions.ts
@@ -4,13 +4,13 @@ import "./panels";
 
 const css = rinss.create({
     inputs: {
-        centerX: true,
+        width: '100%',
         floatTop: 10
     },
     input: {
-        floatLeft: 0,
-        margin: 1,
-        width: 40
+        width: 40,
+        height: 20,
+        overflow: "hidden"
     },
     dimensions: {
         centerX: true,
@@ -22,20 +22,74 @@ const css = rinss.create({
         width: 40,
         margin: 1,
         border: "1px solid black"
+    },
+    dimensionTable: {
+        borderBottom: "1px solid rgb(228,228,228)",
+        width: "100%"
+    },
+    paddingSection: {
+        height: 70,
+        floatTop: 10,
+        centerX: true
+    },
+    paddingTable: {
+        floatLeft: 0
+    },
+    paddingInput: {
+        floatLeft: 10,
+        centerY: true,
+        width: 80
+    },
+    paddingGrid: {
+        width: 20,
+        height: 20,
+        border: "1px solid black",
     }
 });
 
 Vue.component('dimensions-panel',{
     template: `
-    <panel title="Dimensions">
-        <div class="${ css.inputs }">
-            <input class="${css.input}" placeholder="width" v-model="widthRendered"></input>
-            <input class="${css.input}" placeholder="height" v-model="heightRendered"></input>
-        </div>
-        <div class="${ css.dimensions }">
-            <div class="${css.dimension}">{{ widthRendered }}</div>
-            <div class="${css.dimension}">{{ heightRendered }}</div>
-        </div>
+    <panel title="Dimensions" expanded>
+        <table class="${ css.dimensionTable }">
+            <tr>
+                <td align="center">
+                    <input class="${css.input}" placeholder="width" v-model="widthRendered"></input>
+                </td>
+                <td align="center">
+                    <input class="${css.input}" placeholder="height" v-model="heightRendered"></input>
+                </td>
+             </tr>
+             <tr>
+                <td align="center">
+                    <input class="${css.input}" placeholder="none"></input>
+                </td>
+                <td align="center">
+                    <input class="${css.input}" placeholder="none"></input>
+                </td>
+            </tr>
+            <tr>
+                <td align="center">
+                    <div class="${css.input}">{{widthRendered}}</div>
+                </td>
+                <td aligh="center">
+                    <div class="${css.input}">{{heightRendered}}</div>
+                </td>
+            </tr>
+        </table>
+        <div class="${css.paddingSection}">
+            <table class="${css.paddingTable}">
+                <tr>
+                    <td class="${css.paddingGrid}"></td><td class="${css.paddingGrid}"></td><td class="${css.paddingGrid}"></td>
+                </tr>
+                <tr>
+                    <td class="${css.paddingGrid}"></td><td class="${css.paddingGrid}"></td><td class="${css.paddingGrid}"></td>
+                </tr>
+                <tr>
+                    <td class="${css.paddingGrid}"></td><td class="${css.paddingGrid}"></td><td class="${css.paddingGrid}"></td>
+                </tr>
+            </table>
+            <input class="${css.paddingInput}"></input>
+        </div>   
     </panel>
     `,
     data: function() {
